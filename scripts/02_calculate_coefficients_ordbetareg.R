ac3c5b97231f3e9e535bfce86e3410cd183a2456
Jarrett Byrnes <jarrett.byrnes@umb.edu>
2025-10-21 18:34:34 -0400

    add priors

--- scripts/02_calculate_coefficients_ordbetareg.R
+++ scripts/02_calculate_coefficients_ordbetareg.R
@@ -15,6 +15,9 @@ library(purrr)
 library(broom.mixed)
 library(broom)
 library(emmeans)
+library(performance)
+library(glue)
+library(brms)
 
 
 # load the data
@@ -22,13 +25,15 @@ source("scripts/load_data_settings.r")
 
 # helper functions
 get_fitted_values <- function(mod, adf, 
-                              mean_year = mean(seq(min(substrate_long$year), max(substrate_long$year), by = 1))){
+                              mean_year = mean(seq(min(substrate_long$year), max(substrate_long$year), by = 1)),
+                              ...){
   emmeans(mod, 
           specs = ~year_cent*depth,
           epred = TRUE,
           at = list(year_cent = 
                       modelr::seq_range(adf$year_cent, 
-                                        200))) |>
+                                        200)), 
+          ...) |>
     tidy() |>
     mutate(year = year_cent + mean_year)
 }
@@ -39,6 +44,55 @@ get_trends_by_depth <- function(mod){
     tidy()
 }
 
+##
+# evaluate prior choices
+##
+
+# goals of prior - 
+# transitions always likely throoughout time
+# fewer high cover than low cover
+
+deep_prior <- c(set_prior("normal(0,10", class = "b"))
+# one set that kinda works - coef_prior_SD = .01, phi_intercept_prior_SD = 1
+
+prior_mod <- ordbetareg(proportion ~
+             year_cent*depth + (1|subsite),
+           data = subsite_substrate_long,
+           file = glue::glue("models/priorcheck.rds"),
+           file_refit = "on_change",
+           coef_prior_SD = 10,
+           intercept_prior_mean = 0,
+          intercept_prior_SD = 10, 
+           phi_intercept_prior_SD = 1,
+           sample_prior = "only")
+
+get_prior(prior_mod) 
+
+# show prior predictions
+
+
+prior_preds <- tidybayes::add_epred_draws(
+  newdata = modelr::data_grid(subsite_substrate_long,
+                    depth = unique(depth),
+                    year_cent = modelr::seq_range(year_cent, 200)) |>
+    mutate(year = year_cent + mean_year),
+  object = prior_mod,
+  re_formula = NA)
+  
+ggplot(prior_preds,
+       aes(x = year, y = .epred*100, color = depth,
+           group = paste(depth,.draw))) +
+  geom_line(alpha = 0.5) +
+  facet_wrap(vars(forcats::fct_rev(depth)), ncol = 1) +
+  depth_color_scale() +
+  labs(x="", y = "Prior Predicted Percet Cover") +
+  guides(color = "none")
+
+ggsave("figures/prior_predictive_check.jpg",
+       width = 6, height = 10)
+##
+# make the models
+##
 mods_long <- subsite_substrate_long |>
  # debug, try with only a few species
  # filter(species %in% c("scypha_sp", "botryllus_schlosseri")) |>
@@ -52,10 +106,14 @@ mods_long <- subsite_substrate_long |>
   mutate(mod = map(data, ~ordbetareg(proportion ~
                                        year_cent*depth + (1|subsite),
                                       data = .x,
+                                     coef_prior_SD = 10,
+                                     intercept_prior_mean = 0,
+                                     intercept_prior_SD = 10, 
+                                     phi_intercept_prior_SD = 1,
                                      file = glue::glue("models/abundance_models/{.x$sp1[1]}.rds"),
                                      file_refit = 
-                                       #"always")),
-                                       "on_change")), #.x is a placeholder for each nested df
+                                       "always")),
+                                       #"on_change")), #.x is a placeholder for each nested df
          coef = map(mod, ~tidy(.x) |> filter(term == "year_cent")),
          fitted = map2(mod, data, get_fitted_values))
 
@@ -81,3 +139,8 @@ fitted_long <- mods_long |>
   left_join(thermal_data)
 
 write_csv(fitted_long,"data/fitted_long_ordbetareg.csv")
+
+# check model assumptions for each
+walk2(mods_long$mod, mods_long$species, ~check_model(.x) |> plot() |>
+          ggsave(filename = glue("figures/assumptions_timeseries/{.y}.jpg"),
+                 width = 5, height = 8))
